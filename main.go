5bff1eb9b8a8af16fab36c6a73ec0d193d8ccd1f
ferdinand <ferdinand@kali.ferdi>
2022-11-10 12:46:56 +0100

    struct map ex

--- main.go
+++ main.go
@@ -2,5 +2,25 @@ package main
 
 import (
 	"fmt"
-	"os"
 )
+
+type Ville struct {
+	nbHabitants int
+	codepostal  int
+	superficie  int
+}
+
+var villes map[int]Ville
+
+var nbInput int
+var cpInput int
+var spInput int
+
+func main() {
+	fmt.Scanf("%d", &nbInput)
+	fmt.Scanf("%d", &cpInput)
+	fmt.Scanf("%d", &spInput)
+	villes = make(map[int]Ville)
+	villes[0] = Ville{nbInput, cpInput, spInput}
+	fmt.Println(villes[0])
+}
